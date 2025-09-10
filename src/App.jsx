456feda8e4cfe12d1d384a77a59b85cbbc7778c0
jay kumar <jay94588@gmail.com>
2025-09-11 00:25:03 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AddTransaction from './pages/AddTransaction';
 import EditTransaction from './pages/EditTransaction';
@@ -14,10 +14,9 @@ function App() {
         <Navbar />
         <div className="container mx-auto px-4 py-8">
           <Routes>
-            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/add" element={<AddTransaction />} />
             <Route path="/:id/edit" element={<EditTransaction />} />
             <Route path="/:id/delete" element={<DeleteTransaction />} />
