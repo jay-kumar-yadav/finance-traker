2d63b66c6e6d32b305c2518adc30dd4cbada0ce3
jay kumar <jay94588@gmail.com>
2025-09-11 00:06:02 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import AddTransaction from './pages/AddTransaction'
-import EditTransaction from './pages/EditTransaction'
-import DeleteTransaction from './pages/DeleteTransaction'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Navbar from './components/Navbar'
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import AddTransaction from './pages/AddTransaction';
+import EditTransaction from './pages/EditTransaction';
+import DeleteTransaction from './pages/DeleteTransaction';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-      <Router>
-        <div className="min-h-screen bg-gray-100">
-          <Navbar />
-          <div className="container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/" element={    
-                  <Home />
-              } />
-              <Route path="/add" element={
-                
-                  <AddTransaction />
-                
-              } />
-              <Route path="/:id/edit" element={
-                
-                  <EditTransaction />
-                
-              } />
-              <Route path="/:id/delete" element={
-               
-                  <DeleteTransaction />
-                
-              } />
-            </Routes>
-          </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <div className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/add" element={<AddTransaction />} />
+            <Route path="/:id/edit" element={<EditTransaction />} />
+            <Route path="/:id/delete" element={<DeleteTransaction />} />
+          </Routes>
         </div>
-      </Router>
-  )
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
